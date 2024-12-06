2378c650a39816364f64abc189250d30b3b0ceb4
abhi114 <nabhi531@gmail.com>
2024-12-06 16:14:46 +0530

    added custom hide

--- src/Features/tabs/CustomTabBar.tsx
+++ src/Features/tabs/CustomTabBar.tsx
@@ -2,16 +2,23 @@ import { View, Text, StyleSheet } from 'react-native'
 import React, { FC } from 'react'
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
 import { BOTTOM_TAB_HEIGHT, Colors } from '../../utils/Constants'
-import Animated from 'react-native-reanimated'
+import Animated, { useAnimatedStyle } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import ScalePress from '../../components/ui/ScalePress'
 import { HomeTabIcon, LibraryTabIcon, SearchTabIcon } from './TabIcons'
+import { useSharedState } from './SharedContext'
 
 const CustomTabBar:FC<BottomTabBarProps> = (props) => {
     const {state,navigation} = props
     const safeAreaInsets = useSafeAreaInsets()
+    const {translationY} = useSharedState();
+    const animatedStyle = useAnimatedStyle(()=>{
+        return{
+            transform:[{translateY:-translationY.value}]
+        }
+    })
   return (
-    <Animated.View style={[styles.tabBarContainer,{paddingBottom:safeAreaInsets.bottom}]}>
+    <Animated.View style={[styles.tabBarContainer,animatedStyle,{paddingBottom:safeAreaInsets.bottom}]}>
       {
         state.routes.map((route,index)=>{
             const isFocused = state.index === index
